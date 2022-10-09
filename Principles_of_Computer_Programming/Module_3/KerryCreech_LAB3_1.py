62496d97d123261c9219e7fb6ea42b4d165745f1
Kerry Creech <66389478+klcreech@users.noreply.github.com>
2022-10-09 18:10:13 +0100

    Add files via upload

---
+++ Principles_of_Computer_Programming/Module_3/KerryCreech_LAB3_1.py
@@ -0,0 +1,10 @@
+name = input('Please enter your name: ')
+address = input('Please enter your address: ')
+phone = input('Please enter your phone number: ')
+
+print()
+print(name)
+print(address)
+print(phone)
+
+
---
+++ Principles_of_Computer_Programming/Module_3/KerryCreech_LAB3_2.py
@@ -0,0 +1,4 @@
+radius = input('Please the radius of your circle: ')
+area = 3.14*int(radius)**2
+
+print('The area of your circle is: ', area)
