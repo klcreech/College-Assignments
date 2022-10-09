a739e949b1ba94ca5f076ffe560fcb1c3c6a116f
Kerry Creech <66389478+klcreech@users.noreply.github.com>
2022-10-09 17:47:34 +0100

    Add files via upload

---
+++ Principles_of_Computer_Programming/Module_1/Lab_2/KerryCreech_HW2_3.py
@@ -0,0 +1,20 @@
+"""
+Kerry Creech
+Homework 2 – COP1000 
+9/22/2020
+"""
+ 
+# Assigns variable to 5 numbers
+
+a,b,c,d,e = 28, 32, 37, 24, 33
+
+#create sum and average variables
+
+sum = a+b+c+d+e
+avg = sum/5
+
+# Print average
+
+print("The average is:", avg)
+
+
---
+++ Principles_of_Computer_Programming/Module_1/Lab_2/KerryCreech_LAB2_1.py
@@ -0,0 +1,16 @@
+"""
+Kerry Creech
+Lab 1 – COP1000 
+9/22/2020
+"""
+ 
+# This program displays the author’s introduction 
+
+   # Display personal information: 
+print("Kerry Creech")
+print("COP1000 - Fall Term 2020")
+print()
+# Introduction self
+print("Hello, my name is Kerry. I am currently working toward my AS in Programming and Web Development. I have always been interested in coding and artwork I find the field of web development and design gives me the ability to express these desires. I enjoy solving problems graphically and mathematically.")
+
+
