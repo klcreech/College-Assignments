76ab02625f48107dcd32dfbf0230318b3daf662e
Kerry Creech <66389478+klcreech@users.noreply.github.com>
2022-10-09 18:19:14 +0100

    Add files via upload

---
+++ Principles_of_Computer_Programming/Module_6/kerryCreech_LAB6_1.py
@@ -0,0 +1,31 @@
+
+totList = []
+
+sun = float(input("Enter sales total for Sunday:"))
+totList.insert(0, sun)
+
+mon = float(input("Enter sales total for Monday:"))
+totList.insert(1, mon)
+
+tue = float(input("Enter sales total for Tuesday:"))
+totList.insert(2, tue)
+
+wed = float(input("Enter sales total for Wednesday:"))
+totList.insert(3, wed)
+
+thu = float(input("Enter sales total for Thursday:"))
+totList.insert(4, thu)
+
+fri = float(input("Enter sales total for Friday:"))
+totList.insert(5, fri)
+
+sat = float(input("Enter sales total for Saturday:"))
+totList.insert(5, sat)
+
+i = 0
+tot = float(0)
+for x in totList:
+    tot = tot + totList[i]
+    i = i+1
+print(" Your total sales for the week is: ", tot)    
+                 
