12227dd44afa5dfc533c7cf0113cfd428807cb73
Kerry Creech <66389478+klcreech@users.noreply.github.com>
2022-10-09 18:37:07 +0100

    Add files via upload

---
+++ Introduction_to_CPP/Lab_2/Lab_2_creech.cpp
@@ -0,0 +1,23 @@
+//This program will write the name, address and telephone number of the programmer.
+// A C++ program by: Kerry Creech
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+
+{
+
+
+
+
+		cout << "Change of Plans:\t";
+	    return 0; // return a zero indicating success!
+
+
+
+
+}
+
